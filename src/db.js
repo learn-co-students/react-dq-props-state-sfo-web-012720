0a68fe28ab6e617d29cd91cf56f769cdad68f71c
brookebachman00 <yourpetlover4411@yahoo.com>
2020-04-01 11:15:43 -0700

    Done.

--- src/db.js
+++ src/db.js
@@ -1,20 +1,25 @@
+
+
 var offspring = [
   { 
     "id": 1,
     "name": "JeffKatzy Katz",
-    "hobby": "Never gonna give"
+    "hobby": "Never gonna give",
+    "weight": 100
   },
   { 
     "id": 2,
     "name": "Rachel",
-    "hobby": "You up, never gonna"
+    "hobby": "You up, never gonna",
+    "weight": 100
   },
   { 
     "id": 3,
     "name": "Maxwell",
-    "hobby": "let you down"
+    "hobby": "let you down",
+    "weight": 100
   }
 ]
 
 
-export default offspring
+export {offspring}
